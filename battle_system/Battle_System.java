0a8460f19a6a00ce99100990f93d1f8d0f3b0124
madisonkh <31267036+madisonkh@users.noreply.github.com>
2017-10-28 19:27:01 -0400

    Battle System - New Proposal
    Added a proposal for the new handling of the battle system, taking advantage of the different classes rather than doing randomization in the battle system class itself. Formulas will handle most of the calculations and any randomization that might be needed.

--- battle_system/Battle_System.java
+++ battle_system/Battle_System.java
@@ -100,8 +100,10 @@ import character.Weapons;
 			@SuppressWarnings("resource")
 			Scanner input = new Scanner(System.in);
 			Random number = new Random();
-			int action,magic_options=0,physical_options=0;
+			int action, attack, magic_options=0, physical_options=0;
+			char type;
 			double r = Math.random();
+			boolean temp;
 			
 			/*The eName array and the selection array are in a random. 
 			 * I assigned each method with health and a name. 
@@ -115,6 +117,104 @@ import character.Weapons;
 				enemy = selection[rand];
 				enemyName = eName[rand];
 			}
+
+			/* ------------------------------------------
+			NEW BATTLE SYSTEM PROPOSAL: The following is a possibility for how the battle system can be altered.
+
+
+			while (health > 0 && enemy > 0) { // Loop continues until player or enemy health is 0.
+				// Potential statement for determining whether or not the character or enemy moves first could be implemented here.
+
+				public void player_Attack() {
+					System.out.println("Choose your action:" +
+							"\n\t1. Physical Attack" +
+							"\n\t2. Magic Attack" +
+							"\n\t3. Use Item");
+					action = input.nextInt();
+					temp = true;
+
+					while (temp == true) { // Gets further user input for action or initiates.
+						if (action == 1) {
+							System.out.println("Chosse your attack:" +
+									"\n\t1. Punch" +
+									"\n\t2. Weapon");
+							attack = input.nextInt();
+							type = 'p';
+							temp = false
+						} else if (action == 2) {
+							System.out.println("Chosse your spell:" +
+									"\n\t1. Lightning Bolt" +
+									"\n\t2. Flameball");
+							attack = input.nextInt();
+							type = 'm';
+							temp = false;
+						} else if (action == 3) {
+							// Method to open character inventory.
+							System.out.println("Inventory closed. Choose your action:" +
+									"\n\t1. Physical Attack" +
+									"\n\t2. Magic Attack" +
+									"\n\t3. Use Item");
+							action = input.nextInt();
+						} else {
+							System.out.println("Invalid response. Please pick a number from the list above.");
+							action = input.nextInt();
+						}
+					}
+
+					// Physical Attack:
+
+					if (type = 'p') {
+						if (attack == 1) {
+							// Method in Forumulas class for determining punch damage.
+							// Deal X damage to enemy if attack hits.
+							// Report damage information to player.
+						}
+					}
+					else {
+						// Method for determining which weapon to use, could be a method in the inventory class.
+						// Method in Formulas class for determining weapon damage.
+						// Deal X damage to enemy if attack hits.
+						// Report damage information to player.
+					}
+
+					// Magic Attack:
+
+					if (type = 'm') {
+						// Method to check how much magic power the player has left.
+						if (magic_power > 0) {
+							// Method that checks the attack variable to what's in the magic class.
+							// If the character's remaining magic_power is greater than what's needed for that spell, the spell is cast.
+							// If there is not enough magic_power left, the player will have to choose another spell or action.
+							// OR... the spell is cast, but is much weaker depending on the difference.
+							// Deal X damage to enemy if attack hits.
+							// Report damage information to player.
+						}
+						else {
+							System.out.println("You do not have any magic left!");
+						}
+					}
+				}
+
+				public void enemy_Attack() {
+				// Methods for enemy attack calculations from enemy classes, possibly sent to Forumlas class.
+				health -= damage; // Decrease player health if attack hits.
+				}
+			}
+
+			public void battle_End() {
+				if (health > enemy) {
+					System.out.println("You are victorious!");
+					// Methods for winning bonuses.
+				}
+				else {
+					System.out.println("You have been defeated!");
+					// Sends player back to the main menu.
+				}
+			}
+
+
+			END NEW BATTLE SYSTEM
+			------------------------------------------ */
 		
 			while(health > 0){ //If your health is greater than 0, keep going through the loop.
 				System.out.println("\nChoose an Action:  Melee(1)/Magic(2): ");

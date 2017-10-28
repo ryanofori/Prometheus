b9dc7d9c802433ca2130b6c4ed05215551f5bbda
TabiasP <tabiaspittman94@gmail.com>
2017-10-28 16:18:27 -0400

    update

--- game/Main_Class.java
+++ game/Main_Class.java
@@ -1,12 +1,15 @@
 package game;
-import java.util.Scanner;
 import story.Story_Mode;
+import java.util.Scanner;
+
+
+
 public class Main_Class  {
 	public static void main(String [] args){
 		Scanner input = new Scanner(System.in);
 		String response;	//Response holds the users String answer. 
 		
-		Story_Mode Story = new Story_Mode();	//The start and end of the story_Method  
+		Story_Mode Story = new Story_Mode();
 		
 		/*Here I am asking you which character you would like to be. */
 		System.out.println("Are you ready to begin? (Yes/No): ");

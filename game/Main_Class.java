e512bb6e38868f0c162155ce32afaa3c9ad2149b
madisonkh <31267036+madisonkh@users.noreply.github.com>
2017-11-03 11:13:18 -0400

    Character Creation Added to Main_Class
    Character creation now happens at the very beginning of the game.

--- game/Main_Class.java
+++ game/Main_Class.java
@@ -9,6 +9,8 @@ public class Main_Class  {
 		Scanner input = new Scanner(System.in);
 		int response = 0;
 		
+		System.out.println("Welcome to Prometheus!");
+		
 		/*
 		 * There are 3 modes (Story Mode, Tournament and Shopping)
 		 * Have the user select which mode he/she would like to enter
@@ -20,6 +22,51 @@ public class Main_Class  {
 		Shopping shopping = new Shopping();
 		
 		/*Here I am asking you which character you would like to be. */
+		
+		System.out.println("Which character class would you like to play as for this session?:"
+				+ "\n\t1.) Warrior"
+				+ "\n\t1.) Orc"
+				+ "\n\t1.) Wizard"
+				+ "\n\t1.) Elf"
+				+ "\n\t1.) Dwarf");
+		response = input.nextInt();
+		boolean choice = true;
+		while (choice = true) {
+			if(response == 1) {
+				System.out.println("You have chosen to be a warrior!");
+				Character_Warrior character = new Character_Warrior();
+				choice = false;
+			}
+			else if(response == 2) {
+				System.out.println("You have chosen to be an orc!");
+				Character_Orc character = new Character_Orc();
+				choice = false;
+			}
+			else if(response == 3) {
+				System.out.println("You have chosen to be a wizard!");
+				Character_Wizard character = new Character_Wizard();
+				choice = false;
+			}
+			else if(response == 4) {
+				System.out.println("You have chosen to be an elf!");
+				Character_Elf character = new Character_Elf();
+				choice = false;
+			}
+			else if(response == 5) {
+				System.out.println("You have chosen to be a dwarf!");
+				Character_Dwarf character = new Character_Dwarf();
+				choice = false;
+			}
+			else {
+				System.out.println("Invalid Response. Please enter a number from the list above.");
+				response = input.nextInt();
+			}
+		}
+		
+		/* The character has been chosen and will remain the same throughout the game.
+		 * Reference to the character object may need to be passed to each mode in order to save progress from each mode.
+		 */
+	
 		while (true){
 			System.out.println("Please select a mode: Story Mode(1) | Shopping(2) | Tournament(3) | Exit(4)");
 			response = input.nextInt();
